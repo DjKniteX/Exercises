41768984873697a1fe35eac306b334938b74506a
Nikki Xaysanasy <djknitex@outlook.com>
2017-09-04 11:23:35 -0500

    1st commit for assignment 2

---
+++ Assignment 2/js/DateFormatting.js
@@ -0,0 +1,36 @@
+
+//Variables
+var dateFormatter;
+
+//Methods
+function getShortTime(){
+    var shortTime = new Date();
+    var newDate = shortTime.getDate()+7
+    shortTime.setDate(newDate); 
+    return shortTime;
+}
+
+function getLongTime() {
+    return 
+}
+
+function getShortDate() {
+    return 
+}
+
+function getLongDate() {
+    return  
+}
+
+function getShortDateTime(){
+    return
+}
+
+function getLongDateTime(){
+    return
+}
+
+function getExtendedDateTime(){
+    return
+}
+// Console Log
