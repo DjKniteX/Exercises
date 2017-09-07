6baffc9d24bd6fe73c3efa18ad5d3401e44d9491
DjKniteX <djknitex@outlook.com>
2017-09-07 18:37:42 -0500

    1st touch to assignment

---
+++ Exercise14/scripts/main.js
@@ -0,0 +1,24 @@
+//Buttons
+
+document.getElementById("btnUpdate").addEventListener("click", submit);
+
+//Function for Submit Button
+
+function submit() {
+ //Array
+var errorList = [];
+  //Username
+
+  //Email
+
+  //Password
+  
+  //terms & conditions
+
+  //errors
+
+}
+
+//function for reset
+
+function reset() {}
