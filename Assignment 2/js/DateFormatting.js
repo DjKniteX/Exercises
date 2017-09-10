73f8596c37b46819a68d568a360f27933e1e5bce
DjKniteX <djknitex@outlook.com>
2017-09-10 15:28:02 -0500

    adding next hw push

--- Assignment 2/js/DateFormatting.js
+++ Assignment 2/js/DateFormatting.js
@@ -1,146 +1,170 @@
 // Time for JS Assignment //
 
 var dateFormatter = {
-    //Short Time
-    getShortTime: function(date) {
-      var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes()
-        : date.getMinutes();
-      var ampm = date.getHours() < 12 ? "AM" : "PM";
-      var shortTime = date.getHours() % 12 + ":" + minutes + " " + ampm;
-      return shortTime;
-    },
-  
-    //Long Time
-    getLongTime: function(date) {
-      var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes()
-        : date.getMinutes();
-      var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds()
-        : date.getSeconds();
-      var ampm = date.getHours() < 12 ? "AM" : "PM";
-      var longTime =
-        date.getHours() % 12 + ":" + minutes + ":" + seconds + " " + ampm;
-      return longTime;
-    },
-  
-    //Short Date
-    getShortDate: function(date) {
-      var today = new Date();
-      var thisMonth =
-        today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
-      return thisMonth;
-    },
-  
-    //Long Date
-    getLongDate: function(date) {
-      var months = [
-        "January",
-        "Feburary",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-      ];
-      var today = new Date();
-      var month_value = today.getMonth();
-      var longDate =
-        months[month_value] + " " + today.getDate() + ", " + today.getFullYear();
-      return longDate;
-    },
-  
-    //Short Date Time
-  
-    getShortDateTime: function(date) {
-      var today = new Date();
-      var date1 =
-        today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
-      var minutes = today.getMinutes() < 10 ? "0" + today.getMinutes()
-        : today.getMinutes();
-      var seconds = today.getSeconds() < 10 ? "0" + today.getSeconds()
-        : today.getSeconds();
-      var ampm = today.getHours() < 12 ? "AM" : "PM";
-      var time = today.getHours() % 12 + ":" + minutes + " " + ampm;
-  
-      var dateTime = date1 + " " + time;
-  
-      return dateTime;
-    },
-  
-    // Long Date Time
-    getLongDateTime: function(date) {
-      var today = new Date();
-      var months = [
-        "January",
-        "Feburary",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-      ];
-      var month_value = today.getMonth();
-      var date1 =
-        months[month_value] + " " + today.getDate() + ", " + today.getFullYear();
-      var minutes = today.getMinutes() < 10 ? "0" + today.getMinutes()
-        : today.getMinutes();
-      var seconds = today.getSeconds() < 10 ? "0" + today.getSeconds()
-        : today.getSeconds();
-      var ampm = today.getHours() < 12 ? "AM" : "PM";
-      var time = today.getHours() % 12 + ":" + minutes + " " + ampm;
-      var longDate = date1 + " " + time;
-      return longDate;
-    },
-  
-    //Extended Date Time
-    getExtendedDateTime: function(date) {
-      var today = new Date();
-      var months = [
-        "January",
-        "Feburary",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-      ];
-      var days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday"
-      ];
-      var month_value = today.getMonth();
-      var day_value = today.getDay();
-      var day = days[day_value];
-      var date1 =
-        months[month_value] + " " + today.getDate() + ", " + today.getFullYear();
-      var minutes = today.getMinutes() < 10 ? "0" + today.getMinutes()
-        : today.getMinutes();
-      var ampm = today.getHours() < 12 ? "AM" : "PM";
-      var time = today.getHours() % 12 + ":" + minutes + " " + ampm;
-      var extended = day + ", " + date1 + " " + time;
-      return extended;
-    }
-  };
+  //Short Time
+  getShortTime: function(date) {
+     var hours = date.getHours();
+    var minutes = date.getMinutes() < 10
+      ? "0" + date.getMinutes()
+      : date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+    var shortTime = date.getHours() % 12 + ":" + minutes + " " + ampm;
+    return shortTime;
+  },
+
+  //Long Time
+  getLongTime: function(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes() < 10
+      ? "0" + date.getMinutes()
+      : date.getMinutes();
+    var seconds = date.getSeconds() < 10
+      ? "0" + date.getSeconds()
+      : date.getSeconds();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+    var longTime =
+      date.getHours() % 12 + ":" + minutes + ":" + seconds + " " + ampm;
+    return longTime;
+  },
+
+  //Short Date
+  getShortDate: function(date) {
+    var today = new Date();
+    var thisMonth =
+      today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
+    return thisMonth;
+  },
+
+  //Long Date
+  getLongDate: function(date) {
+    var months = [
+      "January",
+      "Feburary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    var today = new Date();
+    var month_value = today.getMonth();
+    var longDate =
+      months[month_value] + " " + today.getDate() + ", " + today.getFullYear();
+    return longDate;
+  },
+
+  //Short Date Time
+
+  getShortDateTime: function(date) {
+     var hours = date.getHours();
+    var today = new Date();
+    var date1 =
+      today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
+    var minutes = today.getMinutes() < 10
+      ? "0" + today.getMinutes()
+      : today.getMinutes();
+    var seconds = today.getSeconds() < 10
+      ? "0" + today.getSeconds()
+      : today.getSeconds();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+    var time = today.getHours() % 12 + ":" + minutes + " " + ampm;
+
+    var dateTime = date1 + " " + time;
+
+    return dateTime;
+  },
+
+  // Long Date Time
+  getLongDateTime: function(date) {
+    var hours = date.getHours();
+    var today = new Date();
+    var months = [
+      "January",
+      "Feburary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    var month_value = today.getMonth();
+    var date1 =
+      months[month_value] + " " + today.getDate() + ", " + today.getFullYear();
+    var minutes = today.getMinutes() < 10
+      ? "0" + today.getMinutes()
+      : today.getMinutes();
+    var seconds = today.getSeconds() < 10
+      ? "0" + today.getSeconds()
+      : today.getSeconds();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+    var time = today.getHours() % 12 + ":" + minutes + " " + ampm;
+    var longDate = date1 + " " + time;
+    return longDate;
+  },
+
+  //Extended Date Time
+  getExtendedDateTime: function(date) {
+    var today = new Date();
+    var months = [
+      "January",
+      "Feburary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    var days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ];
+    var month_value = today.getMonth();
+    var day_value = today.getDay();
+    var day = days[day_value];
+     var hours = date.getHours();
+    var date1 =
+      months[month_value] + " " + today.getDate() + ", " + today.getFullYear();
+    var minutes = today.getMinutes() < 10
+      ? "0" + today.getMinutes()
+      : today.getMinutes();
+    var ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    var time = today.getHours() % 12 + ":" + minutes + " " + ampm;
+    var extended = day + ", " + date1 + " " + time;
+    return extended;
+  }
+};
+
   
   /*
   //bottom of DateFormatting.js
