b4cc1849a225eaf95fc21741d3633f62bba1599a
DjKniteX <djknitex@outlook.com>
2017-10-25 17:25:52 -0500

    added challenge

--- Exercise24/scripts/main.js
+++ Exercise24/scripts/main.js
@@ -1,12 +1,23 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $("#btnSubmit").on('click', function () {
-        $('form').fadeOut('slow', function () {
-            $('#thankyou').fadeIn('slow');
-        });
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
 
-    })
+    window.addEventListener('load', function () {
+        var form = document.getElementById('needs-validation');
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+            $("#btnSubmit").on('click', function () {
+                $('form').fadeOut('slow', function () {
+                    $('#thankyou').fadeIn('slow');
+                });
+            
+            })
+        }, false);
+    }, false);
+})();
 
 
-
-});
